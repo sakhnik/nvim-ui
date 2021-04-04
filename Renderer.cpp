27bc0baf22a1d4b5ef2e0dc556fcb84bcdab24b6
Anatolii Sakhnik <sakhnik@gmail.com>
2021-04-04 11:53:22 +0300

    Try rendering text

--- Renderer.cpp
+++ Renderer.cpp
@@ -4,8 +4,7 @@
 
 
 Renderer::Renderer()
-    : _window(nullptr, SDL_DestroyWindow)
-    , _renderer(nullptr, SDL_DestroyRenderer)
+    : _lines(25)
 {
     SDL_Init(SDL_INIT_VIDEO);
     TTF_Init();
@@ -14,6 +13,9 @@ Renderer::Renderer()
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024,
         768, 0));
     _renderer.reset(SDL_CreateRenderer(_window.get(), -1, 0));
+    _font.reset(TTF_OpenFont("DejaVuSansMono.ttf", 20));
+    TTF_GlyphMetrics(_font.get(), '@', nullptr /*minx*/, nullptr /*maxx*/,
+            nullptr /*miny*/, nullptr /*maxy*/, &_advance);
 }
 
 Renderer::~Renderer()
@@ -24,10 +26,36 @@ Renderer::~Renderer()
 
 void Renderer::Flush()
 {
-    SDL_RenderPresent(_renderer.get());
+    SDL_RenderClear(_renderer.get());
+    for (int row = 0, rowN = _lines.size(); row < rowN; ++row)
+    {
+        for (const auto &col_chunk : _lines[row])
+        {
+            int col = col_chunk.first;
+            const auto &chunk = col_chunk.second;
+
+            int texW = 0;
+            int texH = 0;
+            SDL_QueryTexture(chunk.texture.get(), NULL, NULL, &texW, &texH);
+            SDL_Rect dstrect = { col * _advance, texH * row, texW, texH };
+            SDL_RenderCopy(_renderer.get(), chunk.texture.get(), NULL, &dstrect);
+            SDL_RenderPresent(_renderer.get());
+        }
+    }
+
 }
 
 void Renderer::GridLine(int row, int col, const std::string &text, unsigned hl_id)
 {
     std::cerr << "GridLine " << row << ", " << col << " " << hl_id << " «" << text << "»" << std::endl;
+    SDL_Color fg = { 255, 255, 255 };
+    SDL_Color bg = { 0, 0, 0 };
+
+    _Chunk chunk;
+    chunk.text = text;
+
+    auto surface = PtrT<SDL_Surface>(TTF_RenderUTF8_Shaded(_font.get(),
+        text.c_str(), fg, bg), SDL_FreeSurface);
+    chunk.texture.reset(SDL_CreateTextureFromSurface(_renderer.get(), surface.get()));
+    _lines[row][col] = std::move(chunk);
 }
--- Renderer.hpp
+++ Renderer.hpp
@@ -1,10 +1,20 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <memory>
+#include <map>
+#include <vector>
+
 
 template <typename T>
 using PtrT = std::unique_ptr<T, void(*)(T*)>;
+
+template <typename T>
+PtrT<T> NullPtr(void(*d)(T*))
+{
+    return PtrT<T>(nullptr, d);
+}
 
 class Renderer
 {
@@ -16,6 +26,18 @@ public:
     void GridLine(int row, int col, const std::string &text, unsigned hl_id);
 
 private:
-    PtrT<SDL_Window> _window;
-    PtrT<SDL_Renderer> _renderer;
+    PtrT<SDL_Window> _window = NullPtr(SDL_DestroyWindow);
+    PtrT<SDL_Renderer> _renderer = NullPtr(SDL_DestroyRenderer);
+    PtrT<TTF_Font> _font = NullPtr(TTF_CloseFont);
+    int _advance = 0;
+
+    struct _Chunk
+    {
+        unsigned hl_id = 0;
+        std::string text;
+        PtrT<SDL_Texture> texture = NullPtr(SDL_DestroyTexture);
+    };
+
+    using _LineT = std::map<int, _Chunk>;
+    std::vector<_LineT> _lines;
 };
