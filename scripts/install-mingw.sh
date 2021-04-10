36f047e185f394fee10787e14209432a62826145
Anatolii Sakhnik <sakhnik@gmail.com>
2021-04-10 18:42:38 +0300

    Add cairo and pango to mingw

--- scripts/install-mingw.sh
+++ scripts/install-mingw.sh
@@ -6,6 +6,8 @@ pacman -S --noconfirm \
   mingw-w64-x86_64-libuv \
   mingw-w64-x86_64-SDL2 \
   mingw-w64-x86_64-SDL2_ttf \
+  mingw-w64-x86_64-cairo \
+  mingw-w64-x86_64-pango \
   mingw-w64-x86_64-pkg-config \
   mingw-w64-x86_64-cmake \
   mingw-w64-x86_64-ninja \
