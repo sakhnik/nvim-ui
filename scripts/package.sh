ca812287ea415b452693c45702d4a0470e93391c
Anatolii Sakhnik <sakhnik@gmail.com>
2021-04-11 19:35:20 +0300

    Assign the package version from CMakeLists.txt

--- scripts/package.sh
+++ scripts/package.sh
@@ -6,6 +6,7 @@ fi
 
 mkdir -p dist/{bin,lib,share}
 cp build/nvim-sdl2.exe dist/bin
+touch dist/bin/nvim-sdl2.exe.gui
 
 dlls=$(ntldd.exe -R build/nvim-sdl2.exe | grep -Po "[^ ]+?msys64[^ ]+" | sort -u | grep -Po '[^\\]+$')
 for dll in $dlls; do
@@ -13,4 +14,12 @@ for dll in $dlls; do
 done
 
 cp -r chocolatey/* dist/
-# TODO: adjust the version
+version=$(grep -Po '(?<=set\(VERSION )[^) ]+' CMakeLists.txt)
+if git describe --tags 2>/dev/null | grep -F "$version" >/dev/null 2>&1; then
+  echo "Release version is ${version}"
+else
+  version="${version}-dev${GITHUB_RUN_NUMBER}"
+  echo "Development version is ${version}"
+fi
+
+sed -i "s|VERSION|${version}|" dist/*.nuspec
