413a065b4020c5e207458957858d785a2d7ab5da
Anatolii Sakhnik <sakhnik@gmail.com>
2021-04-12 08:04:58 +0300

    Try adding an icon

---
+++ res/svg2ico.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+convert icon.svg -bordercolor white -border 0 \
+      \( -clone 0 -resize 16x16 \) \
+      \( -clone 0 -resize 32x32 \) \
+      \( -clone 0 -resize 48x48 \) \
+      \( -clone 0 -resize 64x64 \) \
+      -alpha off -colors 256 icon.ico
