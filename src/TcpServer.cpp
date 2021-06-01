257b49b8e31572f286799aa8d27bf52f410a719c
Anatolii Sakhnik <sakhnik@gmail.com>
2021-06-01 07:17:45 +0300

    Receive messages over TCP

--- src/TcpServer.cpp
+++ src/TcpServer.cpp
@@ -19,23 +19,13 @@ TcpServer::TcpServer()
                                      GSocketConnection *,
                                      GObject *source_object,
                                      gpointer user_data);
-    IncomingCbT incoming_cb = [](GSocketService *service,
+    IncomingCbT incoming_cb = [](GSocketService *,
                                  GSocketConnection *connection,
-                                 GObject *source_object,
-                                 gpointer user_data) -> gboolean {
+                                 GObject * /*source_object*/,
+                                 gpointer data) -> gboolean {
         Logger().info("Received incoming connection");
-        //GInputStream *istream = g_io_stream_get_input_stream(G_IO_STREAM(connection));
-        //struct ConnData *data = g_new (struct ConnData, 1);
-
-        //data->connection = g_object_ref (connection);
-
-        //g_input_stream_read_async (istream,
-        //    data->message,
-        //    sizeof (data->message),
-        //    G_PRIORITY_DEFAULT,
-        //    NULL,
-        //    message_ready,
-        //    data);
+        TcpServer *self = reinterpret_cast<TcpServer *>(data);
+        self->_OnConnection(connection);
         return false;
     };
 
@@ -48,38 +38,51 @@ TcpServer::~TcpServer()
     g_socket_service_stop(_service);
 }
 
-//#include <gio/gio.h>
-//#include <glib.h>
-//
-//#define BLOCK_SIZE 1024
-//#define PORT 2345
-//
-//struct ConnData {
-//  GSocketConnection *connection;
-//  char message[BLOCK_SIZE];
-//};
-//
-//void message_ready (GObject * source_object,
-//    GAsyncResult *res,
-//    gpointer user_data)
-//{
-//  GInputStream *istream = G_INPUT_STREAM (source_object);
-//  GError *error = NULL;
-//  struct ConnData *data = user_data;
-//  int count;
-//
-//  count = g_input_stream_read_finish (istream,
-//      res,
-//      &error);
-//
-//  if (count == -1) {
-//    g_error ("Error when receiving message");
-//    if (error != NULL) {
-//      g_error ("%s", error->message);
-//      g_clear_error (&error);
-//    }
-//  }
-//  g_message ("Message was: \"%s\"\n", data->message);
-//  g_object_unref (G_SOCKET_CONNECTION (data->connection));
-//  g_free (data);
-//}
+void TcpServer::_OnConnection(GSocketConnection *connection)
+{
+    ConnData *data = new ConnData(this, connection);
+    _ReceiveMessage(data);
+}
+
+void TcpServer::_ReceiveMessage(ConnData *data)
+{
+    GInputStream *istream = g_io_stream_get_input_stream(G_IO_STREAM(data->connection));
+
+    using MessageReadyT = void (*)(GObject * /*source_object*/,
+            GAsyncResult *,
+            gpointer);
+    MessageReadyT message_ready = [](GObject *, GAsyncResult *res, gpointer data) {
+        ConnData *conn_data = reinterpret_cast<ConnData *>(data);
+        conn_data->tcp_server->_MessageReady(res, conn_data);
+    };
+
+    g_input_stream_read_async(istream,
+                              data->message,
+                              sizeof(data->message),
+                              G_PRIORITY_DEFAULT,
+                              nullptr,
+                              message_ready,
+                              data);
+}
+
+void TcpServer::_MessageReady(GAsyncResult *res, ConnData *data)
+{
+    GInputStream *istream = g_io_stream_get_input_stream(G_IO_STREAM(data->connection));
+
+    GError *error = NULL;
+    int count = g_input_stream_read_finish(istream, res, &error);
+    if (count == -1)
+    {
+        Logger().warn("Error receiving from TCP: {}", error ? error->message : "?");
+        //if (error)
+        //    g_clear_error (&error);
+    }
+    if (!count)
+    {
+        Logger().info("Connection closed");
+        delete data;
+        return;
+    }
+    Logger().info("Message: {}", std::string_view{data->message, size_t(count)});
+    _ReceiveMessage(data);
+}
--- src/TcpServer.hpp
+++ src/TcpServer.hpp
@@ -11,4 +11,26 @@ public:
 private:
     GSocketService *_service;
     guint16 _port;
+
+    struct ConnData
+    {
+        ConnData(TcpServer *tcp_server, GSocketConnection *c)
+            : tcp_server{tcp_server}
+            , connection{g_object_ref(c)}
+        {
+        }
+
+        ~ConnData()
+        {
+            g_object_unref(connection);
+        }
+
+        TcpServer *tcp_server;
+        GSocketConnection *connection;
+        char message[1024];
+    };
+
+    void _OnConnection(GSocketConnection *);
+    void _ReceiveMessage(ConnData *);
+    void _MessageReady(GAsyncResult *, ConnData *);
 };
