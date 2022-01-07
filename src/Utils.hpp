9f49ae48517ae0c0f4463a2bb200b5605874b2df
Anatolii Sakhnik <sakhnik@gmail.com>
2022-01-07 22:40:59 +0200

    Add scope_exit

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <functional>
 
 template <typename T, typename D = void(*)(T*)>
 using PtrT = std::unique_ptr<T, D>;
@@ -16,3 +17,13 @@ PtrT<T, D> MkPtr(T *t, D &&d)
 {
     return PtrT<T, D>(t, std::forward<D>(d));
 }
+
+struct scope_exit
+{
+    using FuncT = std::function<void(void)>;
+    scope_exit(FuncT f) : _f{f} {}
+    ~scope_exit(void) { _f(); }
+
+private:
+    FuncT _f;
+};
