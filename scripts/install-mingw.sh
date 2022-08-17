3d9df7298d4bf925f97e976afffcad2db284e912
Anatolii Sakhnik <sakhnik@gmail.com>
2022-08-17 22:50:57 +0300

    build: install python-markdown in mingw

--- scripts/install-mingw.sh
+++ scripts/install-mingw.sh
@@ -2,5 +2,5 @@
 
 pacman -S --noconfirm \
   mingw-w64-x86_64-{gcc,msgpack-c,libuv,gtk4,pkg-config,gobject-introspection,python-virtualenv,fmt} \
-  mingw-w64-x86_64-{cmake,ninja,meson,ntldd-git} \
+  mingw-w64-x86_64-{cmake,ninja,meson,ntldd-git,python-markdown} \
   zip
