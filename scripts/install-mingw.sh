beda6190e73368856912aa7ebdcdc2af6d35294c
Anatolii Sakhnik <sakhnik@gmail.com>
2022-08-21 09:21:00 +0300

    fix: use external spdlog (#50)

--- scripts/install-mingw.sh
+++ scripts/install-mingw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 pacman -S --noconfirm \
-  mingw-w64-x86_64-{gcc,msgpack-c,libuv,gtk4,pkg-config,gobject-introspection,python-virtualenv,fmt} \
+  mingw-w64-x86_64-{gcc,msgpack-c,libuv,gtk4,pkg-config,gobject-introspection,python-virtualenv,fmt,spdlog} \
   mingw-w64-x86_64-{cmake,ninja,meson,ntldd-git,python-markdown} \
   zip
