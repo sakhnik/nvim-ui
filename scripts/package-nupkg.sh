288e771ed05aed1142f73a8332d1b55a5fedbb9e
Anatolii Sakhnik <sakhnik@gmail.com>
2022-09-20 12:11:48 +0300

    fix: account for mingw prefix remapped to Windows paths (#50)

--- scripts/package-nupkg.sh
+++ scripts/package-nupkg.sh
@@ -6,32 +6,46 @@ fi
 
 cd BUILD
 
+# Ninja maps prefix directly into Windows paths. For instance, / -> c:/tools/msys64
+# We need this path to properly mix in the system dlls.
+msys2_root=`python - <<END
+import sys
+from pathlib import Path
+import re
+
+msys2 = str(Path(sys.executable).parent.parent.parent)
+print(re.sub(r'[^:]+:', '', msys2))
+END`
+
+dist=dist${msys2_root}
+
+rm -rf dist
+
 # Create directories for the distro
-mkdir -p dist/{bin,lib,share/glib-2.0}
+mkdir -p $dist/{bin,lib,share/glib-2.0}
 
 # Make chocolatey create a GUI launcher
-touch dist/bin/nvim-ui.exe.gui
+touch $dist/bin/nvim-ui.exe.gui
 
 # Collect the necessary libraries
 dlls=$(ntldd.exe -R ./src/nvim-ui.exe | grep -Po "[^ ]+?msys64[^ ]+" | sort -u | grep -Po '[^\\]+$')
 for dll in $dlls; do
-  cp /mingw64/bin/$dll dist/bin
+  cp /mingw64/bin/$dll $dist/bin
 done
 
 # Collect the glib2 stuff
-cp -R /mingw64/lib/gdk-pixbuf-2.0 dist/lib/
-cp -R /mingw64/share/glib-2.0/schemas dist/share/glib-2.0/
+cp -R /mingw64/lib/gdk-pixbuf-2.0 $dist/lib/
+cp -R /mingw64/share/glib-2.0/schemas $dist/share/glib-2.0/
 
 # Install the built application and whatever the build system creates
 # Do this after the dependencies installed to properly update gschemas
-DESTDIR=dist meson install --tags=runtime,i18n,gschema
-mv dist/tools/msys64/* dist/
+DESTDIR=dist PREFIX=/ meson install #--tags=runtime,i18n,gschema
 
 # Prepare nuspec
-cp -r ../chocolatey/* dist/
+cp -r ../chocolatey/* $dist/
 if [[ "$NVIM_UI_RELEASE" == "$NVIM_UI_VERSION" ]]; then
-  sed -i "s|PACKAGE_SOURCE_URL|https://github.com/sakhnik/nvim-ui/tree/v${NVIM_UI_VERSION}/chocolatey|" dist/*.nuspec
+  sed -i "s|PACKAGE_SOURCE_URL|https://github.com/sakhnik/nvim-ui/tree/v${NVIM_UI_VERSION}/chocolatey|" $dist/*.nuspec
 else
-  sed -i "s|PACKAGE_SOURCE_URL|https://github.com/sakhnik/nvim-ui/tree/${GITHUB_SHA}/chocolatey|" dist/*.nuspec
+  sed -i "s|PACKAGE_SOURCE_URL|https://github.com/sakhnik/nvim-ui/tree/${GITHUB_SHA}/chocolatey|" $dist/*.nuspec
 fi
-sed -i "s|VERSION|${NVIM_UI_RELEASE}|" dist/*.nuspec
+sed -i "s|VERSION|${NVIM_UI_RELEASE}|" $dist/*.nuspec
